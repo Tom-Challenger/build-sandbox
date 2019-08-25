cb1b659a29f1e0fba98f115e1c8781336f1a6583
Tom-Challenger <it.mail.art@gmail.com>
2019-08-25 17:07:30 +0300

    Обработка JSX

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
-import 'core-js'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-class App {
-	
+const App = () => <p>Hello</p>
 
-	run = async (name = 'World') => {
-		console.log(`Hello ${name}`);
-		console.log([1, 2, [2, 3]].flat())
-	}
-}
-
-const app = new App()
-app.run()
-	.then(()=>console.log('done'))
-	.catch(()=>console.log('error'))
+ReactDOM.render(<App />, document.getElementById('root'))
